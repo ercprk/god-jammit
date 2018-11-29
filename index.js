834668b0f094ac2a01cc03fa656f5e22830fe2fb
Phan Anh Nguyen <phananh@Phans-MacBook-Pro.local>
2018-11-28 23:35:09 -0500

    trying to host client site thing

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('public/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://god-jammit.firebaseio.com"
+});
