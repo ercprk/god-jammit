5e09f956b8911d22d7623163caa8774c981bc5a3
Phan Anh Nguyen <phananh@resnet148-82.medford.tufts.edu>
2018-11-29 21:53:21 -0500

    tryna get firebase to work

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const pug = require('pug')
 const PORT = process.env.PORT || 5000
+var admin = require('firebase-admin');
 /*
 var admin = require('firebase-admin');
 var serviceAccount = require("./god-jammit-firebase-adminsdk-w7yum-a61e9bd5f0.json/");
@@ -15,6 +16,15 @@ express()
   .get('/', (req, res) => res.render('god-jammit/index.html'))
   .get('/new_project', (req, res) => res.render('newProject', { title: "What's up", message: 'YEET!' }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: 'god-jammit',
+    clientEmail: 'firebase-adminsdk-w7yum@god-jammit.iam.gserviceaccount.com',
+    privateKey: '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDU1wn6YNAWV4ZT\n-----END PRIVATE KEY-----\n'
+  }),
+  databaseURL: 'https://god-jammit.firebaseio.com'
+});
 
 /*
 admin.initializeApp({
