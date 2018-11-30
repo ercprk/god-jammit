bb66f21bfca819d30172daac91b02020424bf09e
Phan Anh Nguyen <phananh@resnet148-82.medford.tufts.edu>
2018-11-29 21:09:08 -0500

    Temp removed firebase

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-var admin = require('firebase-admin');
-var serviceAccount = require("path/to/god-jammit-firebase-adminsdk-w7yum-a61e9bd5f0.json");
+//var admin = require('firebase-admin');
+//var serviceAccount = require("path/to/god-jammit-firebase-adminsdk-w7yum-a61e9bd5f0.json");
 
 express()
   .use(express.static(path.join(__dirname, 'god-jammit')))
@@ -11,6 +11,7 @@ express()
   .get('/', (req, res) => res.render('god-jammit/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+/*
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://god-jammit.firebaseio.com"
