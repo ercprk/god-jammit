652700edd1830876b9a95297b7da41b72ad1118d
Phan Anh Nguyen <phananh@resnet148-82.medford.tufts.edu>
2018-11-29 21:26:28 -0500

    removed firebase for Jackie

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+/*
 var admin = require('firebase-admin');
 var serviceAccount = require("/Users/phananh/Desktop/Fall 2018 /Comp 20/god-jammit/node-js-getting-started/god-jammit-firebase-adminsdk-w7yum-a61e9bd5f0.json/");
+*/
 
 express()
   .use(express.static(path.join(__dirname, 'god-jammit')))
@@ -11,7 +13,7 @@ express()
   .get('/', (req, res) => res.render('god-jammit/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-
+/*
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://god-jammit.firebaseio.com"
@@ -22,3 +24,4 @@ var ref = db.ref("restricted_access/secret_document");
 ref.once("value", function(snapshot) {
   console.log(snapshot.val());
 });
+*/
