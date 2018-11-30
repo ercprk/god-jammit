edf6149f62e6baee27e1bb231a8e74a56cd134a8
Phan Anh Nguyen <phananh@resnet148-82.medford.tufts.edu>
2018-11-29 20:42:54 -0500

    Added firebase authentication

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var admin = require('firebase-admin');
+var serviceAccount = require("path/to/god-jammit-firebase-adminsdk-w7yum-a61e9bd5f0.json");
 
 express()
   .use(express.static(path.join(__dirname, 'god-jammit')))
@@ -9,13 +11,24 @@ express()
   .get('/', (req, res) => res.render('god-jammit/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-/*
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://god-jammit.firebaseio.com"
 });
-*/
+
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+
+/*
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: 'god-jammit',
+    clientEmail: 'firebase-adminsdk-w7yum@god-jammit.iam.gserviceaccount.com',
+    privateKey: '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDU1wn6YNAWV4ZT\n-----END PRIVATE KEY-----\n'
+  }),
+  databaseURL: 'https://god-jammit.firebaseio.com'
+});
