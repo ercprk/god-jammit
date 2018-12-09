eb918d562e0affcd42ad65f31f2108073b1319a6
Phan Anh Nguyen <phananh@Phans-MacBook-Pro.local>
2018-12-09 11:51:39 -0500

    MIDI send fix

--- god-jammit/scripts/midi.js
+++ god-jammit/scripts/midi.js
@@ -3,10 +3,10 @@ navigator.requestMIDIAccess()
     .then(onMIDISuccess, onMIDIFailure);
 
 function onMIDISuccess(midiAccess) {
+    socket.emit('receive_note', "hi Jackie");
      for (var input of midiAccess.inputs.values()){
         input.onmidimessage = getMIDIMessage;
         console.log('just played a note');
-        socket.emit('receive_note', "hi Jackie");
     }
 }
 
