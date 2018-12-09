7258826436bda484d6f52d6fda5aba1a916e814d
Jacqueline Nguyen <jnguyen9763@gmail.com>
2018-12-08 22:30:50 -0500

    Implemented socket.io

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const app = express();
 const path = require('path')
 const pug = require('pug')
 const uuid = require('uuid/v4')
@@ -9,25 +10,9 @@ const serviceAccount = require('./serviceAccountKey.json')
 
 //---------Initializes Socket.io instance---------//
 
-<<<<<<< HEAD
-
-var server = require('http').Server(express());
-=======
-var server = require('http').createServer(express());
->>>>>>> b9c79cad9bf8769719a9c556a7664200bdf605c1
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
-
-express().get('/', (req, res) => res.sendFile('index.html', {root: __dirname + '/god-jammit/'}))
-
-// Function that emits message event to clients upon connection
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
 //---------Initializes Firebase db instance---------//
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -36,42 +21,41 @@ admin.initializeApp({
 var db = admin.database();
 
 
-//----------Initializes Express instance-----------//
-express()
+//----------Initializes Express instance
 //https://stackoverflow.com/questions/18165138/res-sendfile-doesnt-serve-javascripts-well
-  .use(express.static(path.join(__dirname, '/god-jammit')))
+app.use(express.static(path.join(__dirname, '/god-jammit')));
   //.use('/styles', express.static(path.join(__dirname, '/god-jammit/styles')))
   //.use('/scripts', express.static(path.join(__dirname, '/god-jammit/scripts')))
   //.use('/images', express.static(path.join(__dirname, '/god-jammit/images')))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(function(req, res, next) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
      res.header("Access-Control-Allow-Origin", "*");
      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
      next();
-  })
-  .set('views', path.join(__dirname, 'views'))
+ });
+app.set('views', path.join(__dirname, 'views'));
   //.engine('html', require('ejs').renderFile)
-  .set('view engine', 'ejs')
+app.set('view engine', 'ejs');
   //.set('view engine', 'html')
-  .set('view engine', 'pug')
+app.set('view engine', 'pug');
+
+
+  //.get('/', (req, res) => res.sendFile('index.html', {root: __dirname + '/god-jammit/'}))
   //https://stackoverflow.com/questions/25270434/nodejs-how-to-render-static-html-with-express-4
-  .post('/', function(req, res) {
+app.post('/', function(req, res) {
     if (JSON.parse(req.body.logged_in)) {
         res.send('/' + uuid());
     }
     else {
         res.send('/login')
     }
-  })
-  .get('/login', (req, res) => res.sendFile('login.html', {root: __dirname + '/god-jammit/'}))
-  .get('/:id', function(req, res) {
+});
+app.get('/login', (req, res) => res.sendFile('login.html', {root: __dirname + '/god-jammit/'}));
+app.get('/:id', function(req, res) {
     res.sendFile('project.html', {root: __dirname + '/god-jammit/'});
-
-
-
-    })
-  .post('/search', function(req, res) {
+});
+app.post('/search', function(req, res) {
       db.ref("projects").orderByChild("made_at").once('value', function(snapshot) {
         var results = {};
         var songs = snapshot.val();
@@ -83,8 +67,8 @@ express()
         res.render('search', {"result": req.body.search, "songs": results});
       });
       //res.sendFile('search.html', {root: __dirname + '/god-jammit/'});
-  })
-  .post('/publish', function(req, res) {
+  });
+app.post('/publish', function(req, res) {
       db.ref("projects/" + req.body.id).set({
           "name": req.body.name,
           "owner": req.body.owner,
@@ -93,13 +77,20 @@ express()
           "audio": req.body.audio
       })
       res.redirect('/')
-  })
+  });
   //.post('/submit', function(req, res) {})
-//https://stackoverflow.com/questions/38541098/how-to-retrieve-data-from-firebase-database
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-<<<<<<< HEAD
-=======
 
+  // Function that emits message event to clients upon connection
+  io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+  });
+
+  io.in('test').emit('message', 'hello');
+//https://stackoverflow.com/questions/38541098/how-to-retrieve-data-from-firebase-database
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 /*
 var db = admin.database();
 var ref = db.ref("restricted_access/secret_document");
@@ -107,4 +98,3 @@ ref.once("value", function(snapshot) {
   console.log(snapshot.val());
 });
 */
->>>>>>> b9c79cad9bf8769719a9c556a7664200bdf605c1
